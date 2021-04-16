40a639284411c3974b26e685cc590d929fc2435d
Ahmed Rafiullah <khattak.ahmed@yahoo.com>
2021-04-16 13:47:14 +0500

    bg-test

--- pages/index.js
+++ pages/index.js
@@ -48,13 +48,16 @@ export default function Home() {
         className="rounded p-4  relative  md-plus:p-8 md-plus:pt-0 "
       >
         <div
-          style={{ zIndex: 0 }}
-          className="absolute top-0 left-0 right-0 bottom-0 h-full"
+          style={{
+            backgroundImage: "url(/images/bg.svg)",
+            backgroundSize: "cover",
+          }}
+          className="absolute top-0 left-0 right-0 bottom-0"
         >
-          <img
-            src="/images/bg.svg"
+          {/* <img
             style={{ width: "100%", height: "100%", objectFit: "cover" }}
-          />
+            src="/images/bg.svg"
+          /> */}
         </div>
         <div
           style={{
